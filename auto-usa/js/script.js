d4ab4707ada9eaebaa464dacca8ad93f378a6dcb
puncoviy <usaspinner@gmail.com>
2022-04-06 14:58:47 +0300

    docs: released slider functionality

---
+++ auto-usa/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = new ChiefSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 555000,
+    });
+  });
